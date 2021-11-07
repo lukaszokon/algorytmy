cecbade91f76847e96bf1e987863310b7aa3f3da
Łukasz Okoń <l.okon@haso.pl>
2021-11-07 10:52:54 +0100

    feature: add bubble_sort function

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def prepare_data(n):
         random_list.append(randint(1, n * 100))
     sorted_list = sorted(random_list)
     sorted_reversed_list = sorted_list[::-1]
-    sorted_with_one_change = sorted_list
+    sorted_with_one_change = sorted_list.copy()
 
     # mid_index = n//2
     # temp_number = sorted_with_one_change[mid_index]
@@ -20,11 +20,20 @@ def prepare_data(n):
     return random_list, sorted_list, sorted_reversed_list, sorted_with_one_change
 
 
-
 def bubble_sort(list_to_sort):
-    pass
+    n = len(list_to_sort)
+    for sorting_round in range(n):
+        for index in range(n-1):
+            if list_to_sort[index] > list_to_sort[index+1]:
+                list_to_sort[index], list_to_sort[index+1] = list_to_sort[index+1], list_to_sort[index]
 
 
 if __name__ == '__main__':
-    n = 1000
+    n = 5
     random_list, sorted_list, sorted_reversed_list, sorted_with_one_change = prepare_data(n)
+    print(random_list, sorted_list, sorted_reversed_list, sorted_with_one_change)
+    bubble_sort(random_list)
+    bubble_sort(sorted_list)
+    bubble_sort(sorted_reversed_list)
+    bubble_sort(sorted_with_one_change)
+    print(random_list, sorted_list, sorted_reversed_list, sorted_with_one_change)
