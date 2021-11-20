db96f10b51932252c06a31a891ebd831add1e396
Łukasz Okoń <l.okon@haso.pl>
2021-11-20 15:12:16 +0100

    feature: add Queue implementation

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+class Queue:
+
+    class _Element:
+        def __init__(self, prev, value):
+            self.value = value
+            self.prev = prev
+
+    def __init__(self):
+        self._first = None
+        self._last = None
+
+    def insert(self, value):
+        new_element = self._Element(None, value)
+        if not self._first:
+            self._first = new_element
+            self._last = new_element
+        else:
+            self._first.prev = new_element
+            self._first = new_element
+
+    def delete(self):
+        if not self._last:
+            return None
+        else:
+            temp_element = self._last
+            self._last = temp_element.prev
+            return temp_element.value
+
 class Stack:
     class _Element:
         def __init__(self, value, prev):
@@ -23,12 +51,14 @@ class Stack:
         return element.value
 
 
+from collections import deque
+
 if __name__ == '__main__':
 
-    stack = Stack()
+    queue = Queue()
     for i in range(5):
         print(i + 1)
-        stack.push(i + 1)
+        queue.insert(i + 1)
 
     for i in range(5):
-        print(stack.pop())
+        print(queue.delete())
